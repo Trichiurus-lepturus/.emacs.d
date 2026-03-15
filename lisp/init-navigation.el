7c2a2801614bf48266e4f2e013acf8039284dc92
Trichiurus-lepturus <144453663+Trichiurus-lepturus@users.noreply.github.com>
2026-03-15 19:24:24 +0800

    consult

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -3,9 +3,48 @@
 (use-package avy
   :bind
   (("C-'" . avy-goto-char-2)
-   ("M-g f" . avy-goto-line))
+   ("M-g g" . avy-goto-line))
   :config
   (setq avy-timeout-seconds 0.2))
+
+(use-package consult
+  :bind
+  (("C-x b" . consult-buffer)
+   ("C-x 4 b" . consult-buffer-other-window)
+   ("C-x 5 b" . consult-buffer-other-frame)
+   ("M-y" . consult-yank-pop)
+   ("M-g f" . consult-flymake)
+   ("M-g M-g" . consult-goto-line)
+   ("M-g o" . consult-outline)
+   ("M-s f" . consult-find)
+   ("M-s d" . consult-fd)
+   ("M-s c" . consult-locate)
+   ("M-s g" . consult-grep)
+   ("M-s M-g" . consult-git-grep)
+   ("M-s r" . consult-ripgrep)
+   ("M-s l" . consult-line)
+   ("M-s M-l" . consult-line-multi)
+   :map isearch-mode-map
+   ("M-e" . consult-isearch-history)
+   ("M-s e" . consult-isearch-history)
+   ("M-s l" . consult-line)
+   ("M-s M-l" . consult-line-multi)
+   :map minibuffer-local-map
+   ("M-s" . consult-history)
+   ("M-r" . consult-history))
+  :init
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+  :config
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult-source-bookmark consult-source-file-register
+   consult-source-recent-file consult-source-project-recent-file
+   :preview-key '(:debounce 0.4 any)))
 
 (provide 'init-navigation)
 
