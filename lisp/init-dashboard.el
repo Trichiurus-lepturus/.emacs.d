9330d63197978fb82d9968231d670433341a0128
Trichiurus-lepturus <144453663+Trichiurus-lepturus@users.noreply.github.com>
2026-06-12 19:02:08 +0800

    fix show dashboard

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -9,8 +9,16 @@
 (use-package dashboard
   :defer t
   :init
-  (setq dashboard-projects-backend 'project-el)
-  (add-hook 'dashboard-before-initialize-hook #'recentf-mode)
+  (dashboard-setup-startup-hook)
+  :config
+  (setq dashboard-projects-backend 'project-el
+        dashboard-center-content t
+        dashboard-show-shortcuts t
+        dashboard-items '((projects . 3)
+                          (recents  . 12))
+        dashboard-footer-messages '("Esc - Meta - Alt - Ctrl - Shift"))
+
+  (defvar sztk-dashboard-daemon-shown nil)
 
   (defun sztk-dashboard-choose-banner ()
     (let ((banner-dir (expand-file-name "banners/"
@@ -23,16 +31,18 @@
                         (t "alt-ascii-art.txt"))))
       (setq dashboard-startup-banner
             (expand-file-name banner-file banner-dir))))
+
   (add-hook 'dashboard-before-initialize-hook
             #'sztk-dashboard-choose-banner)
 
-  (dashboard-setup-startup-hook)
-  :config
-  (setq dashboard-center-content t
-        dashboard-show-shortcuts t
-        dashboard-items '((projects . 3)
-                          (recents  . 12))
-        dashboard-footer-messages '("Esc - Meta - Alt - Ctrl - Shift")))
+  (add-hook
+   'server-after-make-frame-hook
+   (lambda ()
+     (when (and (daemonp)
+                (not sztk-dashboard-daemon-shown))
+       (setq sztk-dashboard-daemon-shown t)
+       (sztk-dashboard-choose-banner)
+       (dashboard-open)))))
 
 (provide 'init-dashboard)
 
